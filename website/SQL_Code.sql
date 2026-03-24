d8be57dc2b18acf30f6290fbc5dbe90528d3ce75
haya-12 <A00048309@TUDublin.ie>
2026-03-24 14:42:30 +0000

    events table

--- website/SQL_Code.sql
+++ website/SQL_Code.sql
@@ -13,3 +13,19 @@ VALUES(1, 'John', 'Doe', 1234);
 
 INSERT INTO students
 VALUES(2, 'Jane', 'Doe', 5678);
+
+INSERT INTO students
+VALUES(3, 'Patric', 'jane', 1274);
+
+INSERT INTO students
+VALUES(4, 'Ahmed', 'Dal', 9678);
+
+create table events (
+  event_id INT PRIMARY KEY,
+  event_name VARCHAR(255),
+  event_date DATE,
+  event_time TIME,
+  event_location VARCHAR(255),
+  society_id INT,
+/*  FOREIGN KEY (society_id) REFERENCES departments(dept_id) */
+)
