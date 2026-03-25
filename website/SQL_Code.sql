f7f50783b4ef9629cd91e53190ab39fe01bd6093
Bushra38 <d24126712@mytudublin.ie>
2026-03-25 11:03:22 +0000

    Soceities table

--- website/SQL_Code.sql
+++ website/SQL_Code.sql
@@ -29,3 +29,26 @@ create table events (
   society_id INT,
 /*  FOREIGN KEY (society_id) REFERENCES departments(dept_id) */
 )
+
+CREATE TABLE societies (
+    society_id SERIAL PRIMARY KEY,
+    society_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    description TEXT,
+    meeting_day VARCHAR(20),
+    contact_email VARCHAR(100)
+);
+
+INSERT INTO societies (society_name, category, description, meeting_day, contact_email)
+VALUES
+('Gaming Society', 'Entertainment', 'A place for gamers to connect, compete, and have fun.', 'Friday', 'gaming@college.ie'),
+
+('Tech Society', 'Academic', 'For students interested in programming, AI, and new technologies.', 'Wednesday', 'tech@college.ie'),
+
+('Sports Society', 'Fitness', 'Organises sports activities and competitions for students.', 'Monday', 'sports@college.ie'),
+
+('Art Society', 'Creative', 'A creative space for painting, drawing, and design.', 'Thursday', 'art@college.ie'),
+
+('Music Society', 'Creative', 'For students who enjoy playing instruments and performing music.', 'Tuesday', 'music@college.ie'),
+
+('Islamic Society', 'Cultural', 'A society that supports Muslim students through events, talks, and community activities.', 'Friday', 'isoc@college.ie');
---
+++ website/meeting_report.md
@@ -0,0 +1,39 @@
+# Meeting Report – Student Companion Website Project
+
+**Date:** 24 March 2026  
+**Time:** 2:00 PM
+
+---
+
+## Attendees
+- Bouchra 
+- Haya  
+- Victoria
+- Peace 
+
+---
+
+## Key Points Discussed
+- Agreed on project theme: **Student Companion Website**
+- Discussed main features of the website
+- Focused on student services and campus life
+- Planned database structure and relationships
+- Assigned initial responsibilities to team members
+
+---
+
+## Tables Decided
+- **Societies** Bouchra
+- **Events** (linked to Societies) Haya
+- **Support Services**Victoria
+- **News**Peace
+
+---
+
+## Decisions Made
+- Societies will be connected to Events (one-to-many relationship)
+- Each team member will create at least one webpage
+- Each member will their SQL for the table and Victoria will add them all into one subabase project. 
+-each member need to come up with their own idea for website page
+
+---
